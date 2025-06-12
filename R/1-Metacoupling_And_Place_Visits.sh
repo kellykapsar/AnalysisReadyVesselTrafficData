08b6241264af983298d93876e017f18b3af57130
kellykapsar <kelly.kapsar@gmail.com>
2025-06-12 12:29:21 -0400

    Correct file name.

--- R/1-Metacoupling_And_Place_Visits.sh
+++ R/1-Metacoupling_And_Place_Visits.sh
@@ -15,5 +15,5 @@ arg_list=(
 # Loop through each pair and submit a job
 for pair in "${arg_list[@]}"; do
   type=$(echo $pair | awk '{print $1}')
-  sbatch 1-AISSubset_BeringStrait_VoyageLineCreation.SB "$type"
+  sbatch 1-Metacoupling_And_Place_Visits.SB "$type"
 done
