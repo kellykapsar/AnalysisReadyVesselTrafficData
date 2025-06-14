efeca22a397c4d597749d70520ed8b3ee8dfff40
kellykapsar <kelly.kapsar@gmail.com>
2025-06-14 14:10:11 -0400

    Consolidate raster generation call into one script with different combos for each subset.

--- R/2-Generate_Rasters.R
+++ R/2-Generate_Rasters.R
@@ -1,7 +1,6 @@
 
 # Load Libraries
 library(spatstat)
-library(maptools)
 library(tidyverse)
 library(ggplot2)
 library(sf)
@@ -10,17 +9,19 @@ library(terra)
 
 # ---- SETUP ----
 
+setwd("/mnt/home/kapsarke/Documents/AnalysisReadyVesselTrafficData/R")
+
 # Read in analysis functions 
-source("1-2-AnalysisFunctions.R")
+source("./1-2-AnalysisFunctions.R")
 
 # Projection (Alaska Albers)
 AA <- "+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 
 # Destination to save files
-output_dir <- paste0("../Data_Processed/Raster_meta/")
+output_dir <- paste0("/mnt/research/CSIS/AIS/Data_Processed_Metacoupling/Raster_Seasonal_Meta/")
 
 # Source of vector files
-dsn <-"../Data_Processed/Vector_Vessels/"
+dsn <-"/mnt/research/CSIS/AIS/Data_Processed_Metacoupling/Vector_Vessels/"
 
 # Mask outside of AIS bounds 
 ais_mask <- st_read( "../Data_Raw/ais_reshape.shp")
@@ -36,16 +37,18 @@ study <- st_read("../Data_Raw/StudyArea_ModifiedBerkman.shp") %>%
 cellsize <- 4000
 
 # Derive all combinations of output rasters 
-month <- stringr::str_pad(1:12, 2, pad = "0")
+# month <- stringr::str_pad(1:12, 2, pad = "0")
+season <- c("winter", "spring", "summer", "fall")
 yr <- commandArgs(trailingOnly = TRUE)
+# 
+df <- expand.grid(season, yr)
+colnames(df) <- c("season", "year")
 
-df <- expand.grid(month, yr)
-colnames(df) <- c("month", "year")
-
-process_meta_type_rasters(df,
-                          dsn,
-                          study,
-                          cellsize,
-                          ais_mask,
-                          land_mask,
-                          output_dir)
+rasterize_ais(df,
+  dsn,
+  output_dir, 
+  cellsize, 
+  ais_mask, 
+  land_mask, 
+  timescale = "seasonal", 
+  subset = "meta_type")
