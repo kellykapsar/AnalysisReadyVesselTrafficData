5a60266dfbc4da1fd2f06bfd0489383b16f0f154
kellykapsar <kelly.kapsar@gmail.com>
2025-07-07 15:44:31 -0400

    Create job combos .txt file for submitting array job.

--- R/2-Generate_Rasters.sh
+++ R/2-Generate_Rasters.sh
@@ -1,9 +1,19 @@
-#!/bin/bash --login
-n="ais" # job name
+#!/bin/bash
+n="ais"
 
-# Note that 98 is the number of pieces the BT IDs were split up into. 
-# Should probably not hardcode this...
-for i in {2015..2024}
-do
-  sbatch --job-name=$n.$i --output=$n.$i.SLURMout --export=year=$i ./2-Generate_Rasters_datamachine.SB
+# Create a list of year-month combos
+> job_list.txt
+for year in {2015..2024}; do
+  for month in {01..12}; do
+    echo "$year $month" >> job_list.txt
+  done
 done
+
+# Count total number of tasks
+total_jobs=$(wc -l < job_list.txt)
+
+# Submit a single array job
+sbatch --array=0-$(($total_jobs - 1)) \
+       --job-name=$n.array \
+       --output=$n.%A_%a.SLURMout \
+       ./2-Generate_Rasters_datamachine.SB
